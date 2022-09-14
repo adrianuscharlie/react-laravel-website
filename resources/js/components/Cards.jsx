ff1453a3ccced838b82210c13da7e70dd184b0a0
adrianuscharlie <adrianus.charlie02@gmail.com>
2022-09-14 22:41:30 +0700

    Improve menu page

--- resources/js/components/Cards.jsx
+++ resources/js/components/Cards.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-export default function Card(props){
+export default function Card(props) {
     console.log(props);
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={props.url} alt={props.name}></img>
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{props.name}</div>
-                <p className="text-base text-gray-700">{props.price}</p>
-            </div>
-            <div className="px-6 py-4">
-                <a className="text-base font-semibold bg-slate-300 py-3 px-8 rounded-full
-                hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">Buy</a>
+        <div className="w-full mb-10 bg-slate-500 lg:w-1/2">
+            <div className="max-w-full rounded overflow-hidden shadow-lg bg-white lg:w-1/3">
+                <img className="w-full" src={props.url} alt={props.name}></img>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{props.name}</div>
+                    <p className="text-base text-gray-700">{props.price}</p>
+                </div>
+                <div className="px-6 py-4 mb-5">
+                    <a
+                        className="text-base font-semibold bg-slate-300 py-3 px-8 rounded-full
+            hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out"
+                    >
+                        Buy
+                    </a>
+                </div>
             </div>
         </div>
-    )
+    );
 }
--- resources/js/pages/Menu.jsx
+++ resources/js/pages/Menu.jsx
@@ -3,8 +3,29 @@ import MenuData from "../components/MenuData";
 import Card from "../components/Cards";
 export default function Menu() {
     return (
-        <section>
-        
+        <section className="pt-36 pb-30 lg:pt-36">
+            <div className="container">
+                <div className="flex flex-wrap">
+                    <div className="w-full item-center justify-center text-center px-20 lg:w-full">
+                        <h1 className="text-base font-semibold text-slate-300 mb-5 md:text-xl">
+                            Let's Order Our Best Menu!
+                        </h1>
+                        <h2 className="text-4xl font-bold text-slate-700 mb-5 md:text-5xl">
+                            Menu
+                        </h2>
+                        <p className="text-base text-black mb-10">
+                            Jl. Kemiri No.165A, Nologaten, Caturtunggal, Kec.
+                            Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta
+                            55281
+                        </p>
+                        <div className="flex flex-wrap lg:px-20">
+                        {MenuData.map((data) => (
+                            <Card url={data.url} name={data.name} price={data.price} />
+                        ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
